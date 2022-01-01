99bfc6478e903b750b8505a62213794d0c00d600
yamanetaisei <swiftmantaisei@gmail.com>
2022-01-02 01:43:02 +0900

    Initial Commit

---
+++ RouterSample-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RouterSample-iOS
+//
+//  Created by 山根大生 on 2022/01/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
