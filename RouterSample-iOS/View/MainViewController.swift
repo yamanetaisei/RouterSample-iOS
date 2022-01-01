f8e12b975fc014ea03a285a499f18a55fa653a00
yamanetaisei <swiftmantaisei@gmail.com>
2022-01-02 01:56:26 +0900

    mainVC追加

---
+++ RouterSample-iOS/View/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  RouterSample-iOS
+//
+//  Created by 山根大生 on 2022/01/02.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
