922fde1db80bff1b98ddb41a9653a3f311c5cb9d
yamanetaisei <swiftmantaisei@gmail.com>
2022-01-02 02:41:00 +0900

    最低限のUIを実装

---
+++ RouterSample-iOS/View/BlueViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BlueViewController.swift
+//  RouterSample-iOS
+//
+//  Created by 山根大生 on 2022/01/02.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+}
--- RouterSample-iOS/View/MainViewController.swift
+++ RouterSample-iOS/View/MainViewController.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet private weak var toBlueButton: UIButton! {
+        didSet {
+            toBlueButton.backgroundColor = .blue
+            toBlueButton.setTitle("Blue", for: .normal)
+            toBlueButton.setTitleColor(.white, for: .normal)
+        }
+    }
+    
+    @IBOutlet private weak var toRedButton: UIButton! {
+        didSet {
+            toRedButton.backgroundColor = .red
+            toRedButton.setTitle("Red", for: .normal)
+            toRedButton.setTitleColor(.white, for: .normal)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
